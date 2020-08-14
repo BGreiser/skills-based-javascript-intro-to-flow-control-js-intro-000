01e678e0b2eceb71a0f5026860081dd004a6754d
alluring-machine-4767 <bgreiser@gmail.com>
2020-08-14 19:16:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,40 +1,21 @@
 function basicTeenager(age) {
-  if (age > 12 && age < 20) {
-    return "You are a teenager!";
-  } // end if
-}; // end function
+  if(age > 12 && age is < 20) {
+    return "You are a teenager!"
+  }
+}//end function
 
 function teenager(age) {
-  if (age > 12 && age < 20) {
-    return "You are a teenager!";
-  } else {
-    return "You are not a teenager";
-  }//end if
-};// end function
+
+}
 
 function ageChecker(age) {
-  if (age < 13) {
-    return "You are a kid";
-  } else if (age > 19) {
-    return "You are a grownup";
-  } else {
-    return "You are a teenager!";
-  }// end if
-};//end function
+
+}
 
 function ternaryTeenager(age) {
-    if (age > 12 && age < 20) {
-     return "You are a teenager";
-   } else {
-     return "You are not a teenager";
-   }//end if
-};//end function
+
+}
 
 function switchAge(age) {
-  if (age > 12 $$ age < 20) {
-    return "You are a teenager";
-  } else {
-    return "You are not a teenager";
-  }// end if
+
 }
-};//end function
